157947e0c48f91a35dab97c23f5ff4ab59de9d92
Ilya Alenin <aleniniy1@it.mos.ru>
2025-12-18 16:14:04 +0300

    HW-38 : webflux : added admin room for chat

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,13 @@ allprojects {
 
     val jetty: String by project
     val freemarker: String by project
+    val wiremock: String by project
+    val r2dbcPostgresql: String by project
+    val jsr305: String by project
+    val sockjs: String by project
+    val stomp: String by project
+    val bootstrap: String by project
+    val springDocOpenapiUi: String by project
 
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
@@ -73,6 +80,14 @@ allprojects {
             dependency("org.eclipse.jetty:jetty-io:$jetty")
             dependency("org.eclipse.jetty:jetty-util:$jetty")
             dependency("org.freemarker:freemarker:$freemarker")
+            dependency("com.github.tomakehurst:wiremock-standalone:$wiremock")
+            dependency("io.r2dbc:r2dbc-postgresql:$r2dbcPostgresql")
+            dependency("com.google.code.findbugs:jsr305:$jsr305")
+            dependency("org.webjars:sockjs-client:$sockjs")
+            dependency("org.webjars:stomp-websocket:$stomp")
+            dependency("org.webjars:bootstrap:$bootstrap")
+            dependency("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenapiUi")
+
         }
     }
 
